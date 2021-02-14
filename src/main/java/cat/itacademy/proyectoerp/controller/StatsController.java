41d06a90d99fc63e2af88e724233b7cae383539a
Stefano <stefano.pola.fargas@gmail.com>
2021-02-14 12:03:26 +0100

    #CREATe API's definition

--- src/main/java/cat/itacademy/proyectoerp/controller/StatsController.java
+++ src/main/java/cat/itacademy/proyectoerp/controller/StatsController.java
@@ -1,18 +1,56 @@
 package cat.itacademy.proyectoerp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+
+//Class responsible to manage the ERPS's statistics
+
 public class StatsController {
 	
 	
- 
+	
+	//ORDER STATS  -> Only accessible by a Client (only by the one who does the request) and all Admins 
+	
+	//ORDER STATS: Orders
+	
+		//Get the number of the client's orders for a period divided by a specific time frame (weekly, monthly, yearly)
 
-	
-	//ORDER STATS
-	
-	
-	//OVERALL STATS
+	    //TODO 
 	
 	
-	//EMPLOYEE STATS	
+		//Get the total number of the client's orders  of a period
+	
+	 	//TODO 
+	
+		//Get the number of all orders of all clients for a period divided by a specific time frame (weekly, monthly, yearly)
+	
+	    //TODO 
+
+
+		//Get the total number of all orders of all clients for a period
+	
+	 	//TODO 
+	
+	
+	//ORDER STATS: Products
+	
+	 //TODO 
+	
+	
+	//ORDER STATS: Expenses
+	
+	 //TODO 
+	
+	
+	//OVERALL STATS -> Only accessible by all Admins 
+	
+	 //TODO 
+	
+	
+	//EMPLOYEE STATS  > Only accessible by all Admins 	 
+	
+	 //TODO 
 	
 	
 	
