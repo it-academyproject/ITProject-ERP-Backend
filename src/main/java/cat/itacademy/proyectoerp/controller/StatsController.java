95fb6594e4165370a14fa085716cdab771d74c78
Stefano <stefano.pola.fargas@gmail.com>
2021-02-14 17:31:28 +0100

    #Create API's definition

--- src/main/java/cat/itacademy/proyectoerp/controller/StatsController.java
+++ src/main/java/cat/itacademy/proyectoerp/controller/StatsController.java
@@ -1,51 +1,143 @@
 package cat.itacademy.proyectoerp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import cat.itacademy.proyectoerp.service.StatsServiceImpl;
 
 
 
 //Class responsible to manage the ERPS's statistics
 
+@RestController
+@RequestMapping ("/stats")
 public class StatsController {
 	
+	@Autowired
+	StatsServiceImpl statsService;
 	
 	
 	//ORDER STATS  -> Only accessible by a Client (only by the one who does the request) and all Admins 
 	
-	//ORDER STATS: Orders
-	
+		
 		//Get the number of the client's orders for a period divided by a specific time frame (weekly, monthly, yearly)
 
 	    //TODO 
 	
 	
-		//Get the total number of the client's orders  of a period
+		//Get the accumulated number of the client's orders 
 	
 	 	//TODO 
 	
-		//Get the number of all orders of all clients for a period divided by a specific time frame (weekly, monthly, yearly)
+	
+	
+	
+	
+	
+		//Get the number of all orders of all clients for a period divided by a specific time frame (weekly, monthly, yearly) -> Only accessible by all Admins
 	
 	    //TODO 
 
+	
+		//Get the number of all orders of all clients for a period divided by a specific time frame (weekly, monthly, yearly) [Page of Clients] -> Only accessible by all Admins
+		
+	    //TODO 
 
-		//Get the total number of all orders of all clients for a period
+		//Get the accumulated number of all orders of all clients -> Only accessible by all Admins
 	
 	 	//TODO 
 	
 	
-	//ORDER STATS: Products
+		//Get the accumulated number of all orders classified by all clients -> Only accessible by all Admins
+		
+	 	//TODO 
+         
+		//Get the accumulated number of all orders classified by  clients [Page of Clients] -> Only accessible by all Admins
+		
+	 	//TODO 
 	
-	 //TODO 
 	
 	
-	//ORDER STATS: Expenses
-	
-	 //TODO 
 	
 	
-	//OVERALL STATS -> Only accessible by all Admins 
+	//OVERALL STATS -> Only accessible by a Client (only by the one who does the request) and all Admins 
 	
-	 //TODO 
+	
+	
+	//OVERALL STATS: Products
+	
+	//TODO: Add methods with Client Pages
+	
+	
+			//Get the number of the different client's products for a period divided by a specific time frame (weekly, monthly, yearly)
+		
+		    //TODO 
+		
+		
+			//Get the total number of the client's products of a period
+		
+		 	//TODO 
+		
+		    
+			//Get the product most purchased by a client for a period divided by a specific time frame (weekly, monthly, yearly)
+			
+		    //TODO
+		
+		
+			//Get the product less purchased by a client for a period divided by a specific time frame (weekly, monthly, yearly)
+			
+		    //TODO
+			
+		
+			//Get the number of all orders of all clients for a period divided by a specific time frame (weekly, monthly, yearly) -> Only accessible by all Admins
+		
+		    //TODO 
+		
+		
+			//Get the total number of all orders of all clients for a period -> Only accessible by all Admins
+		
+		 	//TODO 
+		
+		
+			//Get the product most purchased by all clients for a period divided by a specific time frame (weekly, monthly, yearly) -> Only accessible by all Admins
+			
+		    //TODO
+		
+		
+			//Get the product less purchased by all clients for a period divided by a specific time frame (weekly, monthly, yearly) -> Only accessible by all Admins
+			
+		    //TODO
+		
+			//Get the products never purchased by all clients for a period divided by a specific time frame (weekly, monthly, yearly) -> Only accessible by all Admins
+			
+		    //TODO
+		
+		
+	    
+	
+		//OVERALL STATS: Expenses
+	
+	   //TODO: Add methods with Client Pages
+		
+			//Get the expenses of a client for a period divided by a specific time frame (weekly, monthly, yearly) 
+	
+    		//TODO
+	
+			//Get accumulated expenses of a client 
+			
+			//TODO
+	
+			//Get the expenses of all clients for a period divided by a specific time frame (weekly, monthly, yearly) -> Only accessible by all Admins
+	
+				
+			//TODO
+		
+			//Get the accumulated expenses of clients [Page of Clients] -> Only accessible by all Admins
+	
+			
+			//TODO
 	
 	
 	//EMPLOYEE STATS  > Only accessible by all Admins 	 
