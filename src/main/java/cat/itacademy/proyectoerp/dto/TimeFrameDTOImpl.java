75d49d2735ab78abebe7f9d8dd63036baf778d6f
Stefano <stefano.pola.fargas@gmail.com>
2021-02-15 16:01:53 +0100

    #Create TimeFrameDTOImpl

--- src/main/java/cat/itacademy/proyectoerp/dto/TimeFrameDTOImpl.java
+++ src/main/java/cat/itacademy/proyectoerp/dto/TimeFrameDTOImpl.java
@@ -2,14 +2,11 @@ package cat.itacademy.proyectoerp.dto;
 
 import java.time.LocalDate;
 
-public class TimeFramePageDTOImpl implements IPageDTO, ITimeFrameDTO {
+public class TimeFrameDTOImpl implements ITimeFrameDTO {
 
 	private LocalDate periodInit;
 	private LocalDate periodFinal;
 	private String timeFrame;
-	private int pageNumber;
-	private int pageSize;
-	
 	
 	public LocalDate getPeriodInit() {
 		return periodInit;
@@ -28,19 +25,6 @@ public class TimeFramePageDTOImpl implements IPageDTO, ITimeFrameDTO {
 	}
 	public void setTimeFrame(String timeFrame) {
 		this.timeFrame = timeFrame;
-	}
-	
-	public int getPageNumber() {
-		return pageNumber;
-	}
-	public void setPageNumber(int pageNumber) {
-		this.pageNumber = pageNumber;
-	}
-	public int getPageSize() {
-		return pageSize;
-	}
-	public void setPageSize(int pageSize) {
-		this.pageSize = pageSize;
 	}
 	
 	
