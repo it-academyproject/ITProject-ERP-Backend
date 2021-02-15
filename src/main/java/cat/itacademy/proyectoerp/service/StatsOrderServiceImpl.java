3680f8a2bde370af0b0224d7862c6b204b0f1e0a
Stefano <stefano.pola.fargas@gmail.com>
2021-02-15 18:04:28 +0100

    #Update cambio nombre clase  StatsOrderServiceImpl

---
+++ src/main/java/cat/itacademy/proyectoerp/service/StatsOrderServiceImpl.java
@@ -0,0 +1,17 @@
+package cat.itacademy.proyectoerp.service;
+
+public class StatsOrderServiceImpl implements IStatsService {
+
+	/*@Autowired
+	ClientService clientService;
+	
+	@Autowired
+	OrderService orderService;
+	
+	@Autowired
+	
+	public 
+	
+	*/
+	
+}
