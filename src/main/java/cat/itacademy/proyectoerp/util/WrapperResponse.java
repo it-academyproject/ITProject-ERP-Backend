007fea3de91e3e11182b9755553145b6c83069c2
Stefano <stefano.pola.fargas@gmail.com>
2021-02-15 10:40:00 +0100

    #Create clase WrapperResponse

---
+++ src/main/java/cat/itacademy/proyectoerp/util/WrapperResponse.java
@@ -0,0 +1,19 @@
+package cat.itacademy.proyectoerp.util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+//Class responsible to wrapp de Http response and the status of it
+public class WrapperResponse<T> {
+	private boolean success;
+	private String message;
+	private T object;
+	
+	public ResponseEntity<WrapperResponse<T>> createResponse(){
+		return new ResponseEntity<>(this, HttpStatus.OK);
+	}
+	
+	public ResponseEntity<WrapperResponse<T>> createResponse(HttpStatus status){
+		return new ResponseEntity<>(this,status);
+	}
+}
