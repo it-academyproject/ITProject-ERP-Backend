d56d7f56798d184b207dc98ddf5ce94c9b83096d
Stefano <stefano.pola.fargas@gmail.com>
2021-02-15 16:19:55 +0100

    #Create StatsClientOrderAccumDTO

---
+++ src/main/java/cat/itacademy/proyectoerp/dto/StatsClientOrderAccumDTO.java
@@ -0,0 +1,21 @@
+package cat.itacademy.proyectoerp.dto;
+
+public class StatsClientOrderAccumDTO {
+	
+	private Long idClient;
+	private Integer ordersAccum;
+	public Long getIdClient() {
+		return idClient;
+	}
+	public void setIdClient(Long idClient) {
+		this.idClient = idClient;
+	}
+	public Integer getOrdersAccum() {
+		return ordersAccum;
+	}
+	public void setOrdersAccum(Integer ordersAccum) {
+		this.ordersAccum = ordersAccum;
+	}
+	
+	
+}
