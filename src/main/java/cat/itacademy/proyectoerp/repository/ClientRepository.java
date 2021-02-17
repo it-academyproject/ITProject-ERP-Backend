a985041a9c3186692d4f74eeb15937f836745db5
Rita <rituzka@gmail.com>
2021-02-17 12:50:22 +0100

    changes for documentation

--- src/main/java/cat/itacademy/proyectoerp/repository/ClientRepository.java
+++ src/main/java/cat/itacademy/proyectoerp/repository/ClientRepository.java
@@ -2,14 +2,18 @@ package cat.itacademy.proyectoerp.repository;
 
 
 
+
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 import cat.itacademy.proyectoerp.domain.Client;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client,UUID> {
+	
 
 }
