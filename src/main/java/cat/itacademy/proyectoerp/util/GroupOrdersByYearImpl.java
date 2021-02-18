56bb2c7339754978caddc6fb905bab16aa4aae4b
Stefano <stefano.pola.fargas@gmail.com>
2021-02-18 14:33:26 +0100

    #Created clases to implement IGroupOrdersbyTimeFrame

---
+++ src/main/java/cat/itacademy/proyectoerp/util/GroupOrdersByYearImpl.java
@@ -0,0 +1,15 @@
+package cat.itacademy.proyectoerp.util;
+
+import java.util.List;
+
+import cat.itacademy.proyectoerp.domain.Order;
+
+public class GroupOrdersByYearImpl implements IGroupOrdersByTimeFrame {
+
+	@Override
+	public List<Integer> groupOrders(List<Order> listOrder) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
