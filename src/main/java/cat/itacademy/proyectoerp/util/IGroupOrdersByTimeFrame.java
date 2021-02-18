4b4e01ca1c5f43be774b472e9fcd327634da70ae
Stefano <stefano.pola.fargas@gmail.com>
2021-02-18 14:33:37 +0100

    -

--- src/main/java/cat/itacademy/proyectoerp/util/IGroupOrdersByTimeFrame.java
+++ src/main/java/cat/itacademy/proyectoerp/util/IGroupOrdersByTimeFrame.java
@@ -6,6 +6,6 @@ import cat.itacademy.proyectoerp.domain.Order;
 
 public interface IGroupOrdersByTimeFrame {
 
-	public List<Integer> groupOrders(List<Order> listOrder  );
+	public List<Integer> groupOrders(List<Order> listOrder );
 	
 }
