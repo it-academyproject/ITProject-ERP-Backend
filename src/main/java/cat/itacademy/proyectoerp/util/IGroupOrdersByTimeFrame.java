8b66340a50aa3d3cbcd2dc9b05117c729bf037c9
Stefano <stefano.pola.fargas@gmail.com>
2021-02-18 14:29:48 +0100

    #Create interface IGroupOrdersByTimeFrame

---
+++ src/main/java/cat/itacademy/proyectoerp/util/IGroupOrdersByTimeFrame.java
@@ -0,0 +1,11 @@
+package cat.itacademy.proyectoerp.util;
+
+import java.util.List;
+
+import cat.itacademy.proyectoerp.domain.Order;
+
+public interface IGroupOrdersByTimeFrame {
+
+	public List<Integer> groupOrders(List<Order> listOrder  );
+	
+}
