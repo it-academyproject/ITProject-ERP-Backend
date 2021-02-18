ee4c519413b7b389cae39a853e2a0a6ceeb2ac9d
Stefano <stefano.pola.fargas@gmail.com>
2021-02-18 11:08:47 +0100

    #Create custom method findOrderByClientDate

--- src/main/java/cat/itacademy/proyectoerp/repository/IOrderRepository.java
+++ src/main/java/cat/itacademy/proyectoerp/repository/IOrderRepository.java
@@ -1,13 +1,24 @@
 package cat.itacademy.proyectoerp.repository;
 
+import java.sql.Date;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import cat.itacademy.proyectoerp.domain.Order;
 
 @Repository
 public interface IOrderRepository extends JpaRepository<Order, UUID>{
+	
+
+//ORDER STATS
+	
+//Get the orders of a client created between two dates
+	@Query("SELECT o FROM Order o WHERE o.clientId  = ?1 AND o.date >= ?2 AND o.date =< ?3")
+	Optional<List<Order>> findOrderByClientDate(UUID idClient, Date periodInit, Date periodFinal);
 
 }
