fcd1c4dc06d57749a9482145f08bf022addf82fe
Stefano <stefano.pola.fargas@gmail.com>
2021-02-18 13:07:29 +0100

    #Create method getClientOrdersTimeFrame

--- src/main/java/cat/itacademy/proyectoerp/service/StatsOrderServiceImpl.java
+++ src/main/java/cat/itacademy/proyectoerp/service/StatsOrderServiceImpl.java
@@ -1,5 +1,6 @@
 package cat.itacademy.proyectoerp.service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -9,8 +10,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import cat.itacademy.proyectoerp.domain.Client;
+import cat.itacademy.proyectoerp.domain.Order;
 import cat.itacademy.proyectoerp.dto.StatsClientOrderTimeFrameDTO;
 import cat.itacademy.proyectoerp.dto.TimeFrameDTOImpl;
+import cat.itacademy.proyectoerp.exceptions.ArgumentNotFoundException;
+import cat.itacademy.proyectoerp.exceptions.ArgumentNotValidException;
+import cat.itacademy.proyectoerp.util.OrderValidatorImpl;
 
 @Service
 public class StatsOrderServiceImpl implements IStatsService {
@@ -18,53 +23,50 @@ public class StatsOrderServiceImpl implements IStatsService {
 	@Autowired
 	ClientServiceImpl clientService;
 	
-	//@Autowired
-	//OrderService orderService;
+	@Autowired
+	OrderServiceImpl orderService;
 	
+	@Autowired
+	OrderValidatorImpl orderValidator;
 	
-	
-	
-	//Get the number of the client's orders for a period divided by a specific time frame (weekly, monthly, yearly) 
+	// Get the number of the client's orders for a period divided by a specific time
+	// frame (weekly, monthly, yearly)
 
-   
-	
-	
-	public StatsClientOrderTimeFrameDTO getClientOrdersTimeFrame (UUID idClient, TimeFrameDTOImpl timeFrameDTO) {
-	
-	//Check if the user exists. If not, throw exception ArgumentNotFoundException
-		Client client = clientService.findClientById(idClient).get();
-	
-	//Check if period and time frame are correct	
-		
-		 //TODO 
-		
-	//If the user exists, get all orders of a client of the period
-		
-		//List<Order> listOrdersPeriod = orderService.getOrdersPeriod(UUID idClient, timeFrameDTO.getPeriodInit(), timeFrameDTO.getPeriodFinal() );
-	
-		 //TODO 
-		
-    //Group all orders into a list 
-		
-		 //TODO 
-		
-		if (timeFrameDTO.getTimeFrame().equals("weekly")) {
-			
-			
-		} else if (timeFrameDTO.getTimeFrame().equals("monthly")) {
-			
-			
-		} else {
-			
-			
-			
+	public StatsClientOrderTimeFrameDTO getClientOrdersTimeFrame(UUID idClient, TimeFrameDTOImpl timeFrameDTO) {
+
+		// Validate if the timeFrame is correct
+		if (orderValidator.checkNotValidTimeFrame(timeFrameDTO))
+			throw new ArgumentNotValidException("The specified timeframe is not valid. It must be: W or M or Y ");
+
+		// Check if the user exists. If not, throw exception ArgumentNotFoundException
+		Optional<Client> client = clientService.findClientById(idClient);
+
+		if (client.isEmpty())
+			throw new ArgumentNotFoundException("The client does not exist");
+
+		// If the user exists, get all orders of a client of the period
+		Optional<List<Order>> listOrdersPeriod = orderService.getOrdersClientPeriod(idClient,
+				timeFrameDTO.getPeriodInit(), timeFrameDTO.getPeriodFinal());
+
+		if (listOrdersPeriod.isEmpty())
+			throw new ArgumentNotFoundException("The client has no orders");
+
+		// Group all orders into a list if there are orders		
+
+		if (timeFrameDTO.getTimeFrame().equals("W")) {
+         // TODO: Group Orders in Weeks and count the orders for each week
+		} else if (timeFrameDTO.getTimeFrame().equals("M")) {
+		 // TODO:  Group Orders in Month and count the orders for each month
+		} else if (timeFrameDTO.getTimeFrame().equals("Y")) {
+	     // TODO:  Group Orders in Years and count the orders for each year
 		}
-     //TODO 	
-	//return new StatsClientOrderTimeFrameDTO();
-	
-		return null;
+
+		// Create DTO	
+		// TODO: Implement DTO
+		 return new StatsClientOrderTimeFrameDTO();
+
 		
-		
-	}	
+
+	}
 	
 }
