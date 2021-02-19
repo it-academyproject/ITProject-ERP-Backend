eae82de816cef1fa95ace3edd0bbef84439889ae
Stefano <stefano.pola.fargas@gmail.com>
2021-02-19 15:56:03 +0100

    #Update StatsOrderServiceImpl

--- src/main/java/cat/itacademy/proyectoerp/service/StatsOrderServiceImpl.java
+++ src/main/java/cat/itacademy/proyectoerp/service/StatsOrderServiceImpl.java
@@ -1,5 +1,6 @@
 package cat.itacademy.proyectoerp.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -11,10 +12,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import cat.itacademy.proyectoerp.domain.Client;
 import cat.itacademy.proyectoerp.domain.Order;
+import cat.itacademy.proyectoerp.dto.StatsClientOrderAccumDTO;
 import cat.itacademy.proyectoerp.dto.StatsClientOrderTimeFrameDTO;
 import cat.itacademy.proyectoerp.dto.TimeFrameDTOImpl;
 import cat.itacademy.proyectoerp.exceptions.ArgumentNotFoundException;
 import cat.itacademy.proyectoerp.exceptions.ArgumentNotValidException;
+import cat.itacademy.proyectoerp.util.GroupOrdersByMonthImpl;
+import cat.itacademy.proyectoerp.util.GroupOrdersByWeekImpl;
+import cat.itacademy.proyectoerp.util.GroupOrdersByYearImpl;
+import cat.itacademy.proyectoerp.util.IGroupOrdersByTimeFrame;
 import cat.itacademy.proyectoerp.util.OrderValidatorImpl;
 
 @Service
@@ -29,8 +35,7 @@ public class StatsOrderServiceImpl implements IStatsService {
 	@Autowired
 	OrderValidatorImpl orderValidator;
 	
-	// Get the number of the client's orders for a period divided by a specific time
-	// frame (weekly, monthly, yearly)
+	// Get the number of the client's orders for a period divided by a specific timeframe (weekly, monthly, yearly)
 
 	public StatsClientOrderTimeFrameDTO getClientOrdersTimeFrame(UUID idClient, TimeFrameDTOImpl timeFrameDTO) {
 
@@ -51,22 +56,58 @@ public class StatsOrderServiceImpl implements IStatsService {
 		if (listOrdersPeriod.isEmpty())
 			throw new ArgumentNotFoundException("The client has no orders");
 
-		// Group all orders into a list if there are orders		
+		// Group all orders into a list if there are orders	and count the order for each timeframe	
+		
+			List<Integer> listCountOrder = new ArrayList<Integer>();
+			
+			if (timeFrameDTO.getTimeFrame().equals("W")) {
+				
+		        IGroupOrdersByTimeFrame orderByWeek = new GroupOrdersByWeekImpl();
+		        listCountOrder =  orderByWeek.groupOrders(listOrdersPeriod.get());	
+		        
+			} else if (timeFrameDTO.getTimeFrame().equals("M")) {
+				
+				IGroupOrdersByTimeFrame orderByMonth = new GroupOrdersByMonthImpl();
+		        listCountOrder =  orderByMonth.groupOrders(listOrdersPeriod.get());	
+				
+				
+			} else if (timeFrameDTO.getTimeFrame().equals("Y")) {
+				
+				IGroupOrdersByTimeFrame orderByYear = new GroupOrdersByYearImpl();
+		        listCountOrder =  orderByYear.groupOrders(listOrdersPeriod.get());	
+		
+			}
 
-		if (timeFrameDTO.getTimeFrame().equals("W")) {
-         // TODO: Group Orders in Weeks and count the orders for each week
-		} else if (timeFrameDTO.getTimeFrame().equals("M")) {
-		 // TODO:  Group Orders in Month and count the orders for each month
-		} else if (timeFrameDTO.getTimeFrame().equals("Y")) {
-	     // TODO:  Group Orders in Years and count the orders for each year
-		}
-
-		// Create DTO	
-		// TODO: Implement DTO
-		 return new StatsClientOrderTimeFrameDTO();
+		// Create DTO		
+		 return new StatsClientOrderTimeFrameDTO(idClient,timeFrameDTO.getTimeFrame(), listCountOrder);
 
 		
 
 	}
+	
+	
+	// Get the accumulated number of the client's orders 
+	
+	public StatsClientOrderAccumDTO getClientOrdersAccum (UUID idClient) {
+		
+		// Check if the user exists. If not, throw exception ArgumentNotFoundException
+		Optional<Client> client = clientService.findClientById(idClient);
+
+		if (client.isEmpty())
+			throw new ArgumentNotFoundException("The client does not exist");
+
+		// If the user exists, get all orders of a client 
+		List<Order> listOrders = orderService.findAllOrders();
+
+		if (listOrders.isEmpty())
+			throw new ArgumentNotFoundException("The client has no orders");
+
+		// Create DTO: Count all accumulated orders into a list if there are orders
+		
+		return new StatsClientOrderAccumDTO(idClient,listOrders.size() );
+	}
+	
+	
+	
 	
 }
