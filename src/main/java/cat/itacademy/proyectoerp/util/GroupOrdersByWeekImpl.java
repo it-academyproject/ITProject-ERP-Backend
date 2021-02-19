d79f9ddd8423961ab33eb7406f0a3a6e05c927d8
Stefano <stefano.pola.fargas@gmail.com>
2021-02-19 10:04:57 +0100

    #Create method groupOrders

--- src/main/java/cat/itacademy/proyectoerp/util/GroupOrdersByWeekImpl.java
+++ src/main/java/cat/itacademy/proyectoerp/util/GroupOrdersByWeekImpl.java
@@ -1,15 +1,73 @@
 package cat.itacademy.proyectoerp.util;
 
+import java.sql.Date;
+import java.time.temporal.WeekFields;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 
 import cat.itacademy.proyectoerp.domain.Order;
 
 public class GroupOrdersByWeekImpl implements IGroupOrdersByTimeFrame {
 
 	@Override
-	public List<Integer> groupOrders(List<Order> listOrder) {
-		// TODO Auto-generated method stub
+	public List<Integer> groupOrders(List<Order> listOrders) {
+	
+
+		int counter = 1;		
+		int woy = -1;
+		List<Integer> listCountOrders = new ArrayList<Integer>();
+		
+		
+		Collections.sort(listOrders, new WeekComparator());
+		for (Order order : listOrders) {
+
+			if (woy != getWeekOfYear(order.getDate())) {
+				
+				woy = getWeekOfYear(order.getDate());
+				listCountOrders.add(counter);
+				counter = 1;
+				
+
+			} else {
+
+				counter++;
+			}
+
+			
+		}
+		
 		return null;
+
 	}
+	
+	
+	 public class WeekComparator implements Comparator<Order> {
+
+	       	@Override
+			public int compare(Order o1, Order o2) {
+				
+				Date d1 = o1.getDate();
+				Date d2 = o2.getDate();
+				
+				 int result = getWeekOfYear(d1) - getWeekOfYear(d2);
+		            if (result == 0) {
+		                result = d1.compareTo(d2);
+		            	
+		            	
+		            }
+		            return result;
+			}
+
+	    }
+
+	    protected  int getWeekOfYear(Date date) {
+	        WeekFields wf = WeekFields.of(Locale.getDefault());	     
+	        return date.toLocalDate().get(wf.weekOfYear());
+	    }
+	
+	
 
 }
